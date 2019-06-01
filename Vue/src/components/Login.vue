4d99d3386f0208f732fb9b0ed1f09991d331c2e0
Xue <605534423@qq.com>
2019-06-01 11:05:08 +0800

    vue-cli脚手架

---
+++ Vue/src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login">
+    <form>
+      <h2>登录页面</h2>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login'
+  }
+</script>
+
+<style scoped>
+  .login {
+    height: 500px;
+    width: 300px;
+    margin: 0 auto;
+  }
+</style>
